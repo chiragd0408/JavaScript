222e1d6e4ccc14950e731ac86a3526f6b6bac85a
Chirag Dattatray Ranpise <121669825+chiragd0408@users.noreply.github.com>
2024-01-30 06:23:18 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";  // treat all Js code as newer version
+
+// alert("hello") //we are using nodejs, not browser
+
+console.log(3+3) // code readability should be high
+
+let name = "Chirag"
+let age = 12
+let isLogged = false
+
+// number => 2 to power 53
+// bigInt
+
+// string => ""
+// boolean => 0 and 1 (true ya false)
+// null (stand alone value)
+// undefined => not declared value
+// symbol => unique
+
+// object 
+
+console.log(typeof null); // object 
+console.log(type of Undefined); // undefined
