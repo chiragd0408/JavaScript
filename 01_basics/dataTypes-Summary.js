5767c1d0d3afe618fafb62706ad65c91ed7092d9
Chirag Dattatray Ranpise <121669825+chiragd0408@users.noreply.github.com>
2024-01-30 17:52:48 +0000

    DataTypes-Summary

--- 01_basics/dataTypes-Summary.js
+++ 01_basics/dataTypes-Summary.js
@@ -9,7 +9,7 @@ let userEmail;
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
-console.log(id === anotherId)
+// console.log(id === anotherId)
 // Reference (Non Primtive)
 // Array, Objects,Functions
 
@@ -23,3 +23,23 @@ let myObj = {
 /* let myfunction = function () {
     console.log("Hello World");
 } */
+
+// ****************************************
+
+// stack Memory(Primitive) make as copy
+//  and Heap Memory(Non-Primitive) as a Original
+
+let myYoutubename = "ChiragRanpisedotcom"
+let anothername = myYoutubename
+anothername = "ChaiaurCode"
+console.log(anothername)
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "7548@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "Chirag48@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
