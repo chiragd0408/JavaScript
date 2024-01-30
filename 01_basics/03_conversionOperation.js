ff307e2eaa612c39c393d8a7a969134da71fbdce
Chirag Dattatray Ranpise <121669825+chiragd0408@users.noreply.github.com>
2024-01-30 08:00:33 +0000

    Conversion of DatatTypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,11 +14,31 @@ let valueInnumber = Number(score)
 
 let isLoggedIn = 1
 let boolenIsLoggedIn = Boolean(isLoggedIn)
-console.log(boolenIsLoggedIn);
+// console.log(boolenIsLoggedIn);
 
 // "" => false
 // "Chirag" => true
 
 let Num = 33
 let string = String(Num)
-console.log(Num);
+// console.log(Num);
+
+// *************************Operations******************************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Chirag"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(true);
+
+let num1, num2 ,num3
+num1 = num2 = num3 = 2 + 2
+
+let game = 100
+let y = game++;
+console.log(game);
