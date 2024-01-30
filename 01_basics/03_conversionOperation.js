0fd0db121122f2c8b28012715ebbc4235617e0af
Chirag Dattatray Ranpise <121669825+chiragd0408@users.noreply.github.com>
2024-01-30 07:36:00 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = null
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+let valueInnumber = Number(score)
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn);
+
+// "" => false
+// "Chirag" => true
+
+let Num = 33
+let string = String(Num)
+console.log(Num);
